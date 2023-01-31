2b5a90b00b8722ca5a1f3b0d6d35cec9f34f9f71
Braxx5286 <97646987+bdbrockett21@users.noreply.github.com>
2023-01-31 15:09:59 -0500

    I am done

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,34 +3,78 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        int a = start;
+        int b = stop;
+        String result = "";
+        for (int x = a; x < b; x++) {
+            if (x % 2 == 0) {
+                result += x;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        int a = start;
+        int b = stop;
+        String result = "";
+        for (int x = a; x < b; x++) {
+            if (x % 2 == 1) {
+                result += x;
+            }
+        }
+        return result;
     }
-
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        int a = start;
+        int b = stop;
+        int c = step;
+        String result = "";
+        for (int x = a; x < b; x+=step){
+            result += x*x;
+        }
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
-    }
 
+    public static String getRange(int stop) {
+        int b = stop;
+        String result = "";
+        for (int x = 0; x < b;x++){
+            result += x;
+        }
+        return result;
+    }
     public static String getRange(int start, int stop) {
-        return null;
+        int a = start;
+        int b = stop;
+        String result = "";
+        for (int x = a; x < b;x++){
+            result += x;
+        }
+        return result;
     }
-
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        int num = start;
+        String result = "";
+        while (num < stop) {
+            result += num;
+            num += step;
+        }
+        return result;
     }
-
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        int num = start;
+        String result = "";
+        while (num < stop) {
+            result += (int) Math.pow(num, exponent);
+            num += step;
+        }
+        return result;
     }
 }
+
--- src/main/java/com/github/curriculeon/TableUtilities.java
+++ src/main/java/com/github/curriculeon/TableUtilities.java
@@ -2,14 +2,36 @@ package com.github.curriculeon;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return(getMultiplicationTable(5));
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return(getMultiplicationTable(5));
     }
 
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table ="";
+        int size=tableSize;
+        int row = 1;
+        while(row<=size){
+            int count =1;
+            int num;
+            while(count<-size){
+                num = count*row;
+                if(num<10){
+                    table+= (" "+num+" |");
+                }else if(num<100){
+                    table+= (" "+num+" |");
+                }
+                else{
+                    table += (" "+num+" |");
+                }
+                count++;
+            }
+            table+="\n";
+            row+=1;
+        }
+        return table;
     }
 }
